de5ece501832ba8c5d4225016b484b61d6736408
Steven barnett <steve@sabarnett.co.uk>
2025-02-12 14:00:24 +0000

    Simplify the card selection control.

--- MatchedPairs/MatchedPairs/ScrollViewCarouselView.swift
+++ MatchedPairs/MatchedPairs/ScrollViewCarouselView.swift
@@ -15,46 +15,40 @@ struct ScrollViewCarouselView: View {
     @Binding var scrollID: Int?
     
     let myBundle = Bundle(for: MatchedPairsGameModel.self)
-
+    
     var cardImages: [CardBackgrounds] {
         CardBackgrounds.allCases
     }
     
     var body: some View {
-        NavigationStack {
-            VStack {
-                ScrollView(.horizontal, showsIndicators: false) {
-                    LazyHStack(spacing: 0) {
-                        ForEach(cardImages, id: \.id) { bg in
-                            let bgImage = Image(bg.cardImage,  bundle: myBundle)
-                            VStack {
-                                bgImage
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(maxWidth: .infinity)
-                                    .frame(height: 65)
-                                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                                    .shadow(radius: 10)
-                                    .padding()
-                                
-                                Text(bg.cardTitle)
-                                    .font(.title2)
-                            }
-                            .containerRelativeFrame(.horizontal)
-                            .scrollTransition(.animated, axis: .horizontal) { content, phase in
-                                content
-                                    .opacity(phase.isIdentity ? 1.0 : 0.6)
-                                    .scaleEffect(phase.isIdentity ? 1.0 : 0.6)
-                            }
-                        }
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHStack(spacing: 0) {
+                ForEach(cardImages, id:\.id) { card in
+                    VStack {
+                        Image(card.cardImage, bundle: myBundle)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 60)
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                            .shadow(radius: 10)
+                            .padding()
+                        Text(card.cardTitle)
+                            .font(.title2)
                     }
-                    .scrollTargetLayout()
+                    .containerRelativeFrame(.horizontal)
+                    .scrollTransition(.animated, axis: .horizontal) { content, phase in
+                        content
+                            .opacity(phase.isIdentity ? 1.0 : 0.6)
+                            .scaleEffect(phase.isIdentity ? 1.0 : 0.6)
+                    }
                 }
-                .scrollPosition(id: $scrollID)
-                .scrollTargetBehavior(.paging)
             }
-            .navigationTitle("ScrollView")
+            .scrollTargetLayout()
         }
+        .scrollPosition(id: $scrollID)
+        .scrollTargetBehavior(.paging)
+        
     }
 }
 
